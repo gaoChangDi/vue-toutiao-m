55d1a4940ee43097796edc75f403bbe0562e1761
gaochangdi <2321721643@qq.com>
2021-08-12 18:07:22 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     123
+    <i class="iconfont icon-gengduo"></i>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less' //全局样式
+import Vant from 'vant' //导入所有vant组件
+import 'vant/lib/index.css' //vant样式
+
+Vue.use(Vant) //注册并使用vant
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
